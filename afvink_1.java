60f817a3b9351433aff09a106079806f346fba02
YadeAcem <70939905+YadeAcem@users.noreply.github.com>
2021-09-09 12:10:21 +0200

    Add files via upload

---
+++ afvink_1.java
@@ -0,0 +1,49 @@
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+//opdracht 1
+//public class afvink_1 {
+//    public static void main(String[] args){
+//        System.out.println("Hello world!");
+//    }
+//
+//}
+
+//opdracht 2
+//public class afvink_1 {
+//         public static void main (String[] args){
+//        JOptionPane.showMessageDialog(null, "Hello World! " + args[0]);
+//         }
+//
+//}
+
+//opdracht 3
+//public class afvink_1 extends JFrame {
+//    private JTextField textField;
+//
+//    public static void main(String[] args) {
+//        afvink_1 frame = new afvink_1();
+//        frame.setSize(100, 100);
+//        frame.createGUI();
+//        frame.setVisible(true);
+//    }
+//    private void createGUI(){
+//        setDefaultCloseOperation(EXIT_ON_CLOSE);
+//        Container window = getContentPane();
+//        window.setLayout (new FlowLayout());
+//        textField = new JTextField("hello world!");
+//        window.add(textField);
+//    }
+//}
+
+//opdracht 6
+public class afvink_1 {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+
+}
+
